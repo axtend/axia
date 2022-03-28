655744bc33456ab9566d69fd6fac4884438a152e
Sankar boro <sankar.boro@yahoo.com>
2022-03-28 11:38:55 +0530

    Axctest

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -193,7 +193,7 @@ impl AxlibCli for Cli {
 
 fn set_default_ss58_version(spec: &Box<dyn service::ChainSpec>) {
 	let ss58_version = if spec.is_axctest() {
-		Ss58AddressFormatRegistry::AxiaTestAccount
+		Ss58AddressFormatRegistry::AxctestAccount
 	} else if spec.is_alphanet() {
 		Ss58AddressFormatRegistry::AxlibAccount
 	} else {
